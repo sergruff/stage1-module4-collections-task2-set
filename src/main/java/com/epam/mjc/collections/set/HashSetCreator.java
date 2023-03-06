f0e677c4f98716f837cedb6505a38f59c823e464
sergruff <sergruff@gmail.com>
2023-03-06 22:22:37 +0100

    s1m4collectionsTask2

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -5,5 +5,17 @@ import java.util.List;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        HashSet<Integer> resultSet = new HashSet<>();
+        for (Integer addToSet : sourceList) {
+            resultSet.add(addToSet);
+            if (addToSet % 2 == 0) {
+                while (addToSet % 2 == 0) {
+                    addToSet = addToSet / 2;
+                    resultSet.add(addToSet);
+                }
+            } else resultSet.add(addToSet * 2);
+        }
+        return resultSet;
     }
+
 }
--- src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SubsetOfSquaresCreator.java
@@ -2,8 +2,15 @@ package com.epam.mjc.collections.set;
 
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class SubsetOfSquaresCreator {
     public Set<Integer> createSubsetOfSquares(List<Integer> sourceList, int lowerBound, int upperBound) {
+        Set<Integer> resultSet = new TreeSet<>();
+        for (Integer addToSet : sourceList) {
+            if (addToSet * addToSet >= lowerBound && addToSet * addToSet <= upperBound)
+                resultSet.add(addToSet * addToSet);
+        }
+        return resultSet;
     }
 }
